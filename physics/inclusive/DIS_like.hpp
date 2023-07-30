26690584a9cd5f5986a9c0f8d0c8581357afd84e
dwinney <daniel.winney@gmail.com>
2023-07-31 01:15:28 +0200

    reggeized inclusive implemented!

--- physics/inclusive/DIS_like.hpp
+++ physics/inclusive/DIS_like.hpp
@@ -14,7 +14,8 @@
 
 #include "constants.hpp"
 #include "inclusive_process.hpp"
-#include "sigma_tot/ChristyBosted_F.hpp"
+#include "cgamma.hpp"
+#include "sigma_tot/combined_F.hpp"
 
 namespace jpacPhoto
 {
@@ -26,16 +27,16 @@ namespace jpacPhoto
 
             DIS_like(inclusive_key key, double mX, std::string id = "")
             : raw_inclusive_process(key, mX, id),
-              F1(new_inclusive_function<ChristyBosted_F>(1, kProton)),
-              F2(new_inclusive_function<ChristyBosted_F>(2, kProton))
+              F1(new_inclusive_function<combined_F>(1, kProton)),
+              F2(new_inclusive_function<combined_F>(2, kProton))
             {
                 set_N_pars(4);
             };
 
             DIS_like(inclusive_key key, double mX, int pn, std::string id = "")
             : raw_inclusive_process(key, mX, id),
-              F1(new_inclusive_function<ChristyBosted_F>(1, pn)),
-              F2(new_inclusive_function<ChristyBosted_F>(2, pn))
+              F1(new_inclusive_function<combined_F>(1, pn)),
+              F2(new_inclusive_function<combined_F>(2, pn))
             {
                 set_N_pars(4);
             };
@@ -50,40 +51,55 @@ namespace jpacPhoto
                 _eta   = pars[1];
                 _mEx2  = pars[2]*pars[2];
                 _lam2  = pars[3]*pars[3];
+
+                // Cutoff for regge amplitude
+                _cutoff = exp(1./_lam2/_alphaP) / _alphaP;
             };
 
             // The invariant cross section used S T and M2 as independent vareiables
             inline double invariant_xsection(double s, double t, double M2)
             {
+                if (_regge && -t > _cutoff) return 0;
+
                 update(s, t, M2);
 
-                // Contraction of top and bottom tensors
-                double TdotW; 
-                TdotW  = 3*_F1*_T1;
-                TdotW += (_kdotq*_F1*_T2 + _pdotq*_T1*_F2)      /t;
-                TdotW += (_kdotq*_pdotq  - 2*t*_pdotk)*_T2*_F2/t/t;
-                TdotW += (_pdotk*_pdotk*_T2*_F2 - M2_PROTON*_kdotq*_T1*_F2)/_pdotq/_kdotq;
+                double PTdotW = 0;
+                for (int i = 0; i < _gammas.size(); i++)
+                {
+                    double tpiece, spiece, alpha;
+                    if (!_regge)
+                    {
+                        alpha  = 1;
+                        tpiece = pow(_mEx2 - t, -2);
+                        spiece = 2*_pdotk / M2;
+                    }
+                    else
+                    {
+                        alpha = _alpha0 + _alphaP * t;
+                        tpiece = std::norm(_alphaP * cgamma(1. - alpha) * (1.-exp(-I*PI*alpha))/2);
+                        spiece = s / M2;
+                    };
+
+                    PTdotW += _gammas[i] * tpiece * pow(spiece, 2*alpha - i);
+                };
 
                 // Flux factor
                 double flux = 1/(2*sqrt(s)*qGamma());
-
-                // Exchange propagator
-                double P_ex    = 1/(_mEx2 - t);
 
                 // Form factor in the case of massive vectors
                 double tprime  = t - TMINfromM2( M2_PROTON );
                 double beta_ex = (is_zero(_mEx2) || is_zero(_lam2)) ? 1. : exp(tprime/_lam2)/pow(1-tprime/0.71,-2);
 
                 // in nanobarn!!!!!
-                return flux * pow(P_ex * beta_ex * _eta*_eta * E, 2) * TdotW / (8*PI*PI) / (2.56819E-6); 
+                return flux * pow(beta_ex * _eta*_eta * E, 2) * PTdotW / (8*PI*PI) / (2.56819E-6); 
             };
 
             // Options select proton or neutron target
             static const int kProton = 0, kNeutron = 1;
             inline void set_option (int opt)
             {
-                F1 = new_inclusive_function<ChristyBosted_F>(1, opt);
-                F2 = new_inclusive_function<ChristyBosted_F>(2, opt);
+                F1 = new_inclusive_function<combined_F>(1, opt);
+                F2 = new_inclusive_function<combined_F>(2, opt);
                 return;
             };
 
@@ -105,6 +121,11 @@ namespace jpacPhoto
                 // Hadronic structure function
                 _F1 = F1->evaluate(M2, t);
                 _F2 = F2->evaluate(M2, t);
+
+                // Expansion in cross section in terms of flip amplitudes
+                _gammas[0] =   M2*M2/(4*_pdotq*_kdotq)* _T2*_F2;
+                _gammas[1] = - M2/t * _T2*_F2;
+                _gammas[2] =   3*_F1*_T1 + (_kdotq/t)*_F1*_T2 + (_pdotq/t - M2_PROTON/_pdotq)*_F2*_T1 + _kdotq*_pdotq/t/t*_F2*_T2;
             };  
 
             // If we are reggeized we use the high-energy approximation and use t & x
@@ -122,9 +143,14 @@ namespace jpacPhoto
             double _pdotk, _pdotq, _kdotq;
             double _prefactors, _T1, _T2;
             double _F1, _F2;
+            std::array<double,3> _gammas;
 
             // Proton form factors
             inclusive_function F1, F2;
+
+            // Regge trajectory parameters
+            double _alpha0 = 0.5, _alphaP = 0.9;
+            double _cutoff;
         };
     };
 };
